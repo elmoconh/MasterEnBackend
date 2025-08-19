7fe1545ed99c4c3cc8cc27eb936fa88ade72a355
helmer <helmer@dalt.cl>
2025-08-18 23:25:25 -0400

    [ADD] GEt, PUT, DELETE api requests

--- 03-BackendNodeJS/controllers/project.js
+++ 03-BackendNodeJS/controllers/project.js
@@ -37,4 +37,128 @@ const save = (req, res) => {
 
 }
 
-module.exports = { save };
+//Listar todos los proyectos
+const projects = (req, res) => {
+    Project.find()
+        .then(projects => {
+            if (!projects) {
+                return res.status(404).send({
+                    status: "error",
+                    message: "No hay proyectos para mostrar"
+                })
+            }
+            return res.status(200).send({
+                status: "Success",
+                projects
+            });
+        })
+        .catch(error => {
+            return res.status(500).send({
+                status: "error",
+                message: "Error al listar los proyectos",
+                error
+            });
+        });
+}
+
+
+//consultar un unico proyecto
+const item = (req, res) => {
+    let id = req.params.id
+    console.log(id);
+    Project.findById(id)
+        .then(project => {
+            if (!project) {
+                return res.status(404).send({
+                    status: "error",
+                    message: "No hay proyecto para mostrar"
+                })
+            }
+            return res.status(200).send({
+                status: "Success",
+                project
+            });
+        })
+        .catch(error => {
+            return res.status(500).send({
+                status: "error",
+                message: "Error al listar el proyecto",
+                error
+            });
+        });
+
+}
+//eliminar un proyecto
+const deleteProject = (req, res) => {
+    let id = req.params.id;
+    console.log("buscando el proyecto: ", id);
+    Project.findByIdAndDelete(id)
+        .then(project => {
+
+            if (!project) {
+                return res.status(404).send({
+                    status: "error",
+                    message: "No hay proyecto para mostrar"
+                })
+            }
+
+            return res.status(200).send({
+                status: "success",
+                message: "Eliminado",
+                project
+
+            });
+        })
+        .catch(error => {
+            return res.status(500).send({
+                status: "error",
+                message: "Error al listar el proyecto",
+                error
+            });
+        });
+}
+
+//actualizar un proyecto 
+const updateProject = (req, res) => {
+    let body = req.body;
+
+    if (!body || !body.id) {
+        return res.status(400).send({
+            status: "error",
+            message: "No se encontraron datos para actulizar"
+        });
+    }
+
+    Project.findByIdAndUpdate(body.id, body, { new: true })
+        .then(projectUpdate => {
+            if (!projectUpdate) {
+                return res.status(404).send({
+                    status: "error",
+                    message: "No hay proyecto para mostrar"
+                })
+            }
+            return res.status(200).send({
+                status: "success",
+                message: "Actualizado",
+                projectUpdate
+
+            });
+        })
+        .catch(error => {
+            return res.status(500).send({
+                status: "error",
+                message: "Error al actualizar el proyecto",
+                error
+            });
+        });
+}
+
+
+
+module.exports = {
+    save,
+    projects,
+    item,
+    deleteProject,
+    updateProject
+};
--- 03-BackendNodeJS/routes/project.js
+++ 03-BackendNodeJS/routes/project.js
@@ -8,5 +8,12 @@ const projectController = require("../controllers/project");
 //Definir las rutas
 router.post("/save", projectController.save);
 
+router.get("/projects", projectController.projects);
+
+router.get("/item/:id", projectController.item);
+router.delete("/delete/:id", projectController.deleteProject);
+router.put("/update", projectController.updateProject);
+
+
 // Exportar el router
 module.exports = router;
